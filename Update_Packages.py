922e14418ad5d2bc3e7f01dedc94f1b95ec8ed8d
AsSerge <z00m.serge@gmail.com>
2020-03-02 08:14:22 +0300

    Create work repo

---
+++ Update_Packages.py
@@ -0,0 +1,7 @@
+﻿# Скрипт обновления пакетов Python
+import subprocess as sbp
+import pip
+pkgs = eval(str(sbp.run("pip3 list -o --format=json", shell=True,
+                         stdout=sbp.PIPE).stdout, encoding='utf-8'))
+for pkg in pkgs:
+    sbp.run("pip3 install --upgrade " + pkg['name'], shell=True)
